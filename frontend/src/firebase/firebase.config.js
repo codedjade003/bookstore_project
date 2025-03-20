163f110a91e8d0c73a1f62d439d23a4978a508b7
codedjade003 <132352148+codedjade003@users.noreply.github.com>
2025-03-20 22:45:03 +0000

    final working code

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -1,16 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAdIYvJ73Kf9KkKFnOC7GoFlNxVl2IU6LY",
-  authDomain: "book-store-e1867.firebaseapp.com",
-  projectId: "book-store-e1867",
-  storageBucket: "book-store-e1867.firebasestorage.app",
-  messagingSenderId: "170707538587",
-  appId: "1:170707538587:web:ae6333d7f93ecdbeb52c3e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
